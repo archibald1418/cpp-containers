891da580ad4daf2e992a2bf316c517c69958dc95
ldonita <ldonita@student.21-school.ru>
2022-02-25 23:15:09 +0300

    some notes on templates via Meschtscherin lectures

---
+++ tests/iters.hpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <stack>
+
+#include <iterator>
+
+template<class T,
+        class C = std::random_access_iterator_tag,
+        class Dist = ptrdiff_t,
+        class Pt = T*,
+        class Rt = T&>
+    struct iterator {
+        typedef C iterator_category;
+        typedef T value_type;
+        typedef Dist difference_type;
+        typedef Pt pointer;
+        typedef Rt reference;
+    };
+
+template<class It> // 
+    struct iterator_traits {
+        typedef typename It::iterator_category      iterator_category;
+        typedef typename It::value_type             value_type;
+        typedef typename It::distance_type          distance_type;
+        typedef typename It::pointer                pointer;
+        typedef typename It::reference              reference;
+    };
+
+template <class T>
+    struct iterator_traits<T*>{
+        typedef std::random_access_iterator_tag iterator_category;
+        typedef T value_type;
+        typedef ptrdiff_t distance_type;
+        typedef T* pointer;
+        typedef T& reference;
+    };
+
