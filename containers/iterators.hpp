a260925f29f7cdb1d02d43372d39ea1f4973d318
archibald1418 <liam141810@gmail.com>
2022-06-27 13:18:57 +0100

    moved templates to sep file from iterators.hpp

--- containers/iterators.hpp
+++ containers/iterators.hpp
@@ -23,48 +23,6 @@ template <class InputIterator1, class InputIterator2>
   return (first2 != last2);
 };
 
-template <typename T>
-struct is_const{
-	static const bool value = false;
-	};
-
-template<typename T>
-struct is_const<const T>{
-	static const bool value = true;
-	};
-
-template <bool B, class T = void>
-struct enable_if{};
-
-template<class T>
-struct enable_if<true, T>{
-	typedef T type;
-};
-
-template <class T, T v> struct integral_constant
-{
-  static const T value = v;
-  typedef T value_type;
-  typedef integral_constant<T,v> type;
-  operator T() const { return v; }
-};
-
-template <class T> struct is_integral				: public ft::integral_constant<bool, false> {};
-template <> struct is_integral<bool>				: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<char>				: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<char16_t>			: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<char32_t>			: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<wchar_t>				: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<signed char>			: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<short>				: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<int>					: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<long>				: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<long long>			: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<unsigned char>		: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<unsigned short>		: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<unsigned int>		: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<unsigned long> 		: public ft::integral_constant<bool, true> {};
-template <> struct is_integral<unsigned long long>	: public ft::integral_constant<bool, true> {};
 
 
 	// Explicitly Parametric iterator
