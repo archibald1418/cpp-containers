d107b0e74ac8dd5d9e9729d1748736b3ff21d986
archibald1418 <liam141810@gmail.com>
2022-06-29 16:03:49 +0100

    const vector doesn't have const type; it has const iterator

--- tests/sandbox/const_&_ref/const_test.cpp
+++ tests/sandbox/const_&_ref/const_test.cpp
@@ -1,10 +1,20 @@
 #include <vector>
 using std::vector;
+#include <iostream>
 
 int main(){
-	vector<const int> v;
+	vector<int> v;
 
-	int i = 5;
+	const int i = 5;
 	
 	v.push_back(i);
+
+	std::cout << v.at(0) << std::endl;
+	std::cout << &(*v.begin()) << std::endl;
+	std::cout << &i << std::endl;
+	
+	v[0] = 6;
+
+	std::cout << *v.begin() << std::endl;
+	std::cout << i << std::endl;
 }
--- tests/sandbox/const_&_ref/main.cpp
+++ tests/sandbox/const_&_ref/main.cpp
@@ -1,15 +1,64 @@
-int main(int argc, char **argv)
+#include <iostream>
+#include <map>
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <numeric>
+#include <algorithm>
+
+int& f(int x){
+	int &y = *(new int(x));
+	return y;
+	// битая ссылка, не надо так
+};
+
+int main()
 {
-	int a = 5;
-	int b = 4;
+	
+	// for (int i = 0; i < 20; ++i)
+	// {
+	// 	int a = 5;
+	// 	std::cout << &f(a) << std::endl;
+	// }
 
-	int *pa = &a;
-	int *pb = &b;
+	delete &f(4);
 
-	(void)argc;
-	(void)argv;
+	std::map<int, int> map;
 
-	++pa, ++pb;
+	map[0] = 0;
+	map[1] = 1;
+	map[2] = 2;
+	
+	std::map<int, int>::iterator it = map.begin();
+
+	while (it != map.end()){
+		std::cout << it->first << "\n";
+		++it;
+	}
+
+
+  std::istringstream str("0.1 0.2 0.3 0.4");
+    std::partial_sum(std::istream_iterator<double>(str),
+                      std::istream_iterator<double>(),
+                      std::ostream_iterator<double>(std::cout, ","));
+	
+	std::istream_operator<double> it(str);
+
+	whil
+
+	
+
+	
+
+	// int x = 5;
+	// const int& y = x;
+
+	// int& z = const_cast<int&> (x);
+
+	// z++;
+
+	// std::cout << y << std::endl;
+	// std::cout << z << std::endl;
 
 	return (0);
 }
