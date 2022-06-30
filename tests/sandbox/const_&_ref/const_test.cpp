0d927c9abdcee00c6ab171f0a85c7eb2c07bf239
archibald1418 <liam141810@gmail.com>
2022-06-30 18:08:32 +0100

    const vector forbids non-const iterator

--- tests/sandbox/const_&_ref/const_test.cpp
+++ tests/sandbox/const_&_ref/const_test.cpp
@@ -3,11 +3,15 @@ using std::vector;
 #include <iostream>
 
 int main(){
-	vector<int> v;
+
+	typedef const vector<int> const_vector;
+	const_vector v;
 
 	const int i = 5;
 	
 	v.push_back(i);
+
+	const_vector::const_iterator it = v.begin();
 
 	std::cout << v.at(0) << std::endl;
 	std::cout << &(*v.begin()) << std::endl;
