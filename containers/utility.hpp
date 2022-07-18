520fa61e384306374aaa41fb69b76389ab327e38
archibald1418 <liam141810@gmail.com>
2022-07-18 12:45:17 +0100

    fixed min-max

--- containers/utility.hpp
+++ containers/utility.hpp
@@ -2,17 +2,17 @@
 namespace ft{
 
     template <typename T>
-    inline T min(T& a, T& b){
-        if (a >= b)
-            return a;
-        return b;
+    inline T min(const T& a, const T& b){
+        if (b < a)
+            return b;
+        return a;
     }
 
     template <typename T>
     inline T max(const T& a, const T& b){
-        if (a <= b)
-            return a;
-        return b;
+        if (a < b)
+            return b;
+        return a;
     }
 
 };
