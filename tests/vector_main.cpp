047d981cbb3b415e4745bd00d171abacbf78db88
archibald1418 <liam141810@gmail.com>
2022-07-22 19:49:35 +0100

    passing through vector tests

--- tests/vector_main.cpp
+++ tests/vector_main.cpp
@@ -5,7 +5,7 @@
 #include <cassert>
 #include <stack>
 #include <iterator>
-#include <random>
+
 #include "ft_containers.hpp"
 #include "pick_vector.hpp"
 #include "VectorFactory.hpp"
@@ -19,35 +19,34 @@
 #include "test_resizing.hpp"
 #include "test_iterator_non_members.hpp"
 #include "test_inserters.hpp"
+#include "colors.hpp"
+
 
 using test::VectorFactory;
 using test::factory_type;
 using test::unique_ptr;
 
+
+
 int main(){
-  std::cout << RED << "\nTESTING " << vector_types[MY] << "\n\n" << RESET;
+  std::cout << YELLOW << "\nTESTING " << vector_types[MY] << "\n\n" << RESET;
 
-  test_vector_erase<int>();
-  test_vector_erase<std::string>();
-  test_vector_erase<char>();
-  test_vector_reserve<int>();
-  test_vector_resize_with_default_value<std::string>("spam");
+  // test_vector_erase<int>();
+  // test_vector_erase<std::string>();
+  // test_vector_erase<char>();
+  // test_vector_reserve<int>();
+  // test_vector_resize_with_default_value<std::string>("spam");
   
-  test_vector_sizes(*unique_ptr< vector<int> >(VectorFactory<int>::factory::create()));
+  // test_vector_sizes(*unique_ptr< vector<int> >(VectorFactory<int>::factory::create()));
 
   
-  // vector<int> v1;
-  // vector<int> dummy;
-  // (void)dummy;
-  // create_vector_of_ints(v1, 10);
-  // v1[0] = -1;
+  vector<int> v1;
+  vector<int> dummy;
+  (void)dummy;
+  create_vector_of_ints(v1, 10);
+  v1[0] = -1;
 
-  // test_vector_iterate(v1);
-  // vector<int>::const_iterator last = v.cend() - 1;
-  // // vector<int>::iterator it = v.erase(dummy.end() - 1);
-  // // (void)it;
-  // vector<int>::iterator range_it = v.erase(v.begin(), v.begin());
-  // // (void)range_it;
+  test_iterators(v1);
 
 
 
