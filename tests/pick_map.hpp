b23cd0e9841ab7c0651ce960cc0ee1ce7210d237
archibald1418 <liam141810@gmail.com>
2022-08-02 15:27:00 +0100

    setup tests for map

---
+++ tests/pick_map.hpp
@@ -0,0 +1,26 @@
+#ifndef PICK_MAP_H
+# define PICK_MAP_H
+
+# include <map>
+# include <iterator>
+# include "ft_containers.hpp"
+
+extern const std::string map_types[2] = {
+    std::string("SYS_MAP"),
+    std::string("MY_MAP")
+    };
+
+# ifdef MY
+#  if MY == 1
+    using ft::map;
+#  else
+    using std::map;
+#  endif
+# endif
+
+#ifndef MY
+# define MY 0
+    using std::map;
+# endif
+
+#endif
