5c8e65526eb69d67893759697f4ebbd7a6e05c06
archibald1418 <liam141810@gmail.com>
2022-08-02 15:25:31 +0100

    map typedefs

--- containers/map.hpp
+++ containers/map.hpp
@@ -1,6 +1,25 @@
 #ifndef MAP_H
 # define MAP_H
 
+# include "functional.hpp"
+# include "utility.hpp"
+# include <memory>
 
+namespace ft{
+    template <
+        typename Key,
+        typename T,
+        typename Compare = ft::less<Key>,
+	    typename Alloc = std::allocator<ft::pair<const Key, T> > 
+    >
+    class map
+    {
+        public:
+            typedef Key					        key_type;
+            typedef T					        mapped_type;
+            typedef ft::pair<const Key, T>		value_type;
+            typedef Compare					    keycompare;
+            typedef Alloc					    allocator_type;
+    };
 
 #endif
