530dfb357cbdb0831e69912db4922ab76d6e1ef4
archibald1418 <liam141810@gmail.com>
2022-08-14 20:25:19 +0100

    fixed tree inheritance types

--- containers/node.hpp
+++ containers/node.hpp
@@ -7,108 +7,127 @@
 # include "type_traits.hpp"
 # include "pair.hpp"
 
-template <typename T, template<typename> class NodeType>
-struct Tree;
 
-template <typename T, template<typename> class NodeType>
-struct node_traits
-{
-    typedef NodeType<T>         node_t;
-    typedef T                   value_type;
-    typedef node_t*             pointer;
-}; 
+# include <iostream>
 
-template <typename T, template<typename> class NodeImpl>
-struct BaseNode{
-    typedef node_traits<T, NodeImpl>        traits;
-    typedef typename traits::node_t         node_t;
-    typedef typename traits::value_type     value_type;
-    typedef typename traits::pointer        pointer;
+namespace ft {
 
-    protected:
-        pointer     left;
-        pointer     right;
-        pointer     parent;
-        value_type  item;
-
-    public:
-        pointer Left(){
-            return dynamic_cast<pointer>(this->left);
-        }
-        pointer Right(){
-            return dynamic_cast<pointer>(this->right);
-        }
-        pointer Parent(){
-            return static_cast<pointer>(this->parent);
-        }
+    template <typename T, template<typename> class NodeType>
+    struct node_traits
+    {
+        typedef NodeType<T>         node_t;
+        typedef T                   value_type;
+        typedef node_t*             pointer;
+    }; 
 
 
-        BaseNode() : left(NULL), right(NULL), parent(NULL), item(T()){};
-        BaseNode(const T& item, pointer left = NULL, pointer right = NULL, pointer parent = NULL)
-        :
-        left(left), right(right), parent(parent), item(item){};
-        BaseNode(const BaseNode& other)
-        :
-        left(other.left), right(other.right), parent(other.parent), item(other.item){};
+    template <typename T, template<typename> class NodeImpl>
+    struct BaseNode
+    {
+        typedef ft::node_traits<T, NodeImpl>        traits;
+        typedef typename traits::node_t             node_t;
+        typedef typename traits::value_type         value_type;
+        typedef typename traits::pointer            pointer;
 
-        virtual value_type& get(){ return this->item;};
+        protected:
+            pointer     left;
+            pointer     right;
+            pointer     parent;
+            value_type  item;
 
-        virtual ~BaseNode(){
-            delete left;
-            delete right;
-        }
+        public:
 
-        static pointer create(const value_type& item, 
-        pointer left = NULL, pointer right = NULL, pointer parent = NULL){
-            return new node_t(item, left, right, parent);
-        }
-        static pointer clone(const node_t& node){
-            return new node_t(node);
-        }
-    
-};
+            pointer& Left(){
+                return this->left;
+            }
+            pointer& Right(){
+                return this->right;
+            }
+            pointer& Parent(){
+                return this->parent; // шиза полная
+            }
 
-template <typename T>
-struct Node : public BaseNode<T, Node>{};
+            
 
-template <typename T>
-struct AVLNode : public BaseNode<T, AVLNode>
-{
-    /* 
-        CRTP, with Tree and Node metaclasses, 
-        hold shared functionality of the children,
-        take designated child class as a template template parameter
-        and generating traits based on the parameter
-    */
+            BaseNode() : left(NULL), right(NULL), parent(NULL), item(T()){};
+            BaseNode(const T& item, pointer left = NULL, pointer right = NULL, pointer parent = NULL)
+            :
+            left(left), right(right), parent(parent), item(item){};
+            BaseNode(const BaseNode& other)
+            :
+            left(other.left), right(other.right), parent(other.parent), item(other.item){};
 
-    typedef BaseNode<T, AVLNode>          __base;
-    typedef typename __base::traits         traits;
-    
-    typedef typename traits::node_t         node_t;
-    typedef typename traits::value_type     value_type;
-    typedef typename traits::pointer        pointer;
-    
-    private:
-        int balance_factor;
-            // Insertion and deletion are in charge of this field
-    public:
-        AVLNode() : __base(), balance_factor(0){}
-        AVLNode(const value_type& item,
-        pointer left = NULL, pointer right = NULL, int balfac = 0, pointer parent = NULL)
-        :
-        __base(item, left, right, parent), balance_factor(balfac){};
-        AVLNode(const node_t& other)
-        :
-        __base(other), balance_factor(other.balance_factor){};
+            virtual value_type& get(){ return this->item;};
 
-        int get_balance_factor(){return this->balance_factor;}
+            virtual ~BaseNode(){
+                std::cout << "Calling base node dtor" << std::endl;
+                delete left;
+                delete right;
+            }
 
-        static pointer create(const value_type& item,
-        pointer left = NULL, pointer right = NULL, int balfac = 0, pointer parent = NULL){
-            new AVLNode(item, left, right, parent, balfac);
-        }
+            static pointer create(const value_type& item, 
+            pointer left = NULL, pointer right = NULL, pointer parent = NULL){
+                return new node_t(item, left, right, parent);
+            }
+            static pointer clone(const node_t& node){
+                return new node_t(node);
+            }
         
-};
+    };
 
+            template <typename T>
+        struct Node : public BaseNode<T, Node>{};
+
+        template <typename T>
+        struct AVLNode : public BaseNode<T, AVLNode>
+        {
+            /* 
+                CRTP, with Tree and Node metaclasses, 
+                hold shared functionality of the children,
+                take designated child class as a template template parameter
+                and generating traits based on the parameter
+            */
+
+            typedef BaseNode<T, AVLNode>          __base;
+            typedef typename __base::traits         traits;
+            
+            typedef typename traits::node_t         node_t;
+            typedef typename traits::value_type     value_type;
+            typedef typename traits::pointer        pointer;
+            
+            private:
+                int balance_factor;
+                    // Insertion and deletion are in charge of this field
+            public:
+                AVLNode() : __base(), balance_factor(0){}
+                AVLNode(const value_type& item,
+                pointer left = NULL, pointer right = NULL, int balfac = 0, pointer parent = NULL)
+                :
+                __base(item, left, right, parent), balance_factor(balfac){};
+                AVLNode(const node_t& other)
+                :
+                __base(other), balance_factor(other.balance_factor){};
+                
+                virtual ~AVLNode(){
+                    
+                };
+
+
+                int get_balance_factor(){return this->balance_factor;}
+
+                static pointer create(const value_type& item,
+                pointer left = NULL, pointer right = NULL, int balfac = 0, pointer parent = NULL){
+                    return (new AVLNode(item, left, right, balfac, parent));
+                }
+                
+        };
+        
+        // TODO: allocator
+            // TODO: iterator
+
+
+
+
+}
 
 #endif
--- containers/tree.hpp
+++ containers/tree.hpp
@@ -17,156 +17,186 @@
 namespace ft{
 
     template <
-        class Node, 
-        typename Predicate = ft::less<typename Node::value_type> > // value_compare for insertion
+    // TODO: extend this to handle mappings (kv pairs with comparators)
+        typename T,
+        class Alloc = std::allocator<T>, 
+        class Predicate = ft::less<T> // value_compare for insertion
+    > 
     struct tree_traits
     {
-        typedef Node                            node_t;
-        typedef typename node_t::pointer        node_pointer;
-        typedef typename node_t::value_type     value_type;
+        typedef T                               value_type;
+        typedef T*                              pointer;
         typedef Predicate                       value_compare; // oblivious to whether pair is a mapping or not
+        // typedef Predicate                    key_compare; -> 'set traits'
+
+        value_compare comp; // comparator should be created before it could be called
+
+        tree_traits(value_compare& comp) : comp(comp){};
+        tree_traits(){};
+        ~tree_traits(){};
     };
 
-
-        template <class T, template<typename> class NodeType>
-    struct BaseTree
+        template <
+            class T,
+            template<typename> class NodeType
+        >
+    struct BaseTree : public tree_traits<T>
     {
-        typedef tree_traits< NodeType<T> >      traits;
+        typedef tree_traits<T>                  traits;
+        typedef NodeType<T>                     node_t;
         typedef BaseTree<T, NodeType>           tree_t;
-        typedef typename traits::node_t         node_t;
-        typedef typename traits::node_pointer   node_pointer;
         typedef typename traits::value_type     value_type;
         typedef typename traits::value_compare  value_compare;
-        
-        // TODO: allocator
-            // TODO: iterators
+        typedef value_type                      V;
 
-        node_t* root;
-        
-        node_t* Root(){
-            return root;
-        }
-    
-        // TODO: coplien form, destructors, allocators(?)
+        protected:
+            node_t* root;
+        public:
+            node_t*& Root(){
+                return root->parent;
+            }
 
-        BaseTree(const value_compare& comp){
-            (void)comp;
-        };
+            void Init(){
+                root = node_t::create(V());
+                root->Left() = NULL;
+                root->Right() = NULL;
+                root->Parent() = NULL;
+            }
 
-        node_t* search(node_t* node, const value_type& key)
-        {
-            while (node != NULL && node->key != key)
+            // TODO: coplien form, destructors, allocators(?)
+
+            BaseTree(const value_compare& comp) : traits(comp)
             {
-                if (node->key > key)
-                    node = node->left;
+                Init();
+            };
+            BaseTree(node_t*& root){
+                if (!root)
+                    Init();
                 else
+                    this->root = node_t::clone(*root);
+            }
+            ~BaseTree(){
+                delete root;
+            }
+
+            node_t* search(node_t* node, const value_type& key)
+            {
+                while (node != NULL && node->key != key)
+                {
+                    if (node->key > key)
+                        node = node->left;
+                    else
+                        node = node->right;
+                }
+                return node;
+            }
+
+            node_t* tree_min(node_t* node){
+                while (node->left != NULL)
+                    node = node->left;
+                return node;
+            }  
+            node_t* tree_max(node_t* node){
+                while (node->right != NULL)
                     node = node->right;
+                return node;
             }
-            return node;
-        }
+            node_t* tree_next(node_t* node){
+                // Searching for next item in an ordered sequence
 
-        node_t* tree_min(node_t* node){
-            while (node->left != NULL)
-                node = node->left;
-            return node;
-        }  
-        node_t* tree_max(node_t* node){
-            while (node->right != NULL)
-                node = node->right;
-            return node;
-        }
-        node_t* tree_next(node_t* node){
-            // Searching for next item in an ordered sequence
+                // Next item is the next greatest to the current item
+                // So it's in the right subtree
+                if (node->right)
+                    return tree_min(node->right);
 
-            // Next item is the next greatest to the current item
-            // So it's in the right subtree
-            if (node->right)
-                return tree_min(node->right);
-
-            // Otherwise go up and search the first parent which is also a left node
-            node_t* node_parent = node->parent;
-            while (node_parent != NULL and node == node_parent->right){
-                node = node_parent;
-                node_parent = node_parent->parent;
+                // Otherwise go up and search the first parent which is also a left node
+                node_t* node_parent = node->parent;
+                while (node_parent != NULL and node == node_parent->right){
+                    node = node_parent;
+                    node_parent = node_parent->parent;
+                }
+                return node;
             }
-            return node;
-        }
-        node_t* tree_prev(node_t* node){
-            // Same, but searching for rightmost child
-            if (node->left)
-                return tree_max(node->left);
+            node_t* tree_prev(node_t* node){
+                // Same, but searching for rightmost child
+                if (node->left)
+                    return tree_max(node->left);
 
-            node_t* node_parent = node->parent;
-            while (node_parent != NULL and node == node_parent->left){
-                node = node_parent;
-                node_parent = node_parent->parent;
+                node_t* node_parent = node->parent;
+                while (node_parent != NULL and node == node_parent->left){
+                    node = node_parent;
+                    node_parent = node_parent->parent;
+                }
+                return node;
             }
-            return node;
-        }
 
-        node_pointer Inserter(const value_type& item)
-        {
-            node_pointer tree_root = static_cast<node_pointer>(Root());
-            node_pointer new_node = node_t::create(item, NULL, NULL);
-            this->Insert(tree_root, new_node);
-            return new_node;
+            node_t* Inserter(const value_type& item)
+            {
+                node_t* tree_root = static_cast<node_t*>(Root());
+                node_t* new_node = node_t::create(item, NULL, NULL);
+                this->Insert(tree_root, new_node);
+                return new_node;
+            };
+            void Insert(node_t**& root, node_t**& new_node)
+            {
+                if (!root)
+                    return ;
+                (void)new_node;
+                // TODO: use comparators for correct insertion
+                
+            }
+
+            // node_t* Deleter(const value_type& item){
+            //     // throw std::exception("Not Implemented");
+            //     // return this->Delete(tree_root, new_node);
+            //     (void)item;
+            //     return NULL;
+            // }
+
+            // Happy tree friends
+            template <typename U>
+            friend int height(node_t* root);
+
+            template <typename U>
+            friend bool is_avl_balanced(node_t* root);
+
+            // These functions are essential for balancing,
+            // because they need access to protected fields
+
         };
-        virtual void Insert(node_pointer*& root, node_pointer*& new_node)
-        {
-            if (!root)
-                return ;
-            (void)new_node;
-            // TODO: use comparators for correct insertion
-            
-        }
-
-        node_pointer Deleter(const value_type& item){
-            // throw std::exception("Not Implemented");
-            // return this->Delete(tree_root, new_node);
-            (void)item;
-            return NULL;
-        }
-
-
-    // Happy tree friends
-    template <typename U>
-    friend int height(NodeType<U>* root);
-
-    template <typename U>
-    friend bool is_avl_balanced(AVLNode<U>* root);
-
-    // These functions are essential for balancing,
-    // because they need access to protected fields
-
-    };
 
     
     template <typename T>
     struct AVLTree : public BaseTree<T, AVLNode>
     {
+        typedef tree_traits<T>                          traits;
+        typedef BaseTree<T, AVLNode>                    __base;
+        typedef typename traits::value_compare          value_compare;
+        typedef typename traits::value_type             value_type;
+        typedef typename __base::node_t                 node_t;
         
-        typedef BaseTree<T, AVLNode>            __base;
-        typedef tree_traits< AVLNode<T> >       traits;
-        
-        typedef typename traits::node_t         node_t;
-        typedef typename traits::node_pointer   node_pointer;
-        typedef typename traits::value_type     value_type;
-        
+
+
+        AVLTree(const value_compare& comp) : __base(comp){};
+        AVLTree(AVLNode<T>*& root) : __base(root){};
+
         private:
+
             node_t* get_node(const value_type& item, 
-            node_t* lptr, node_t* rptr){
+            node_t* lptr, node_t* rptr)
+            {
                 return node_t::create(item, lptr, rptr);
             }
 
             void Insert(const T& item){
-                node_pointer tree_root = static_cast<node_pointer>(this->Root());
-                node_pointer new_node = node_t::create(item, NULL, NULL);
+                node_t* tree_root = static_cast<node_t*>(this->Root());
+                node_t* new_node = node_t::create(item, NULL, NULL);
 
                 // int revise_balance_factor = 0; // ????
             }
-            void Delete(const T& item){
-                (void)item;
-            }
+            // void Delete(const T& item){
+            //     (void)item;
+            // }
 
 // Rotations
             // int reviseBalanceFactor = 0; // flag for height-fixing
@@ -180,8 +210,8 @@ namespace ft{
     };
 
 
-}
 
+}
 
 
 
