4a6d42c6f5ec1d1ab7077f869b12fae78214abad
archibald1418 <liam141810@gmail.com>
2022-09-18 01:22:31 +0100

    linux infra works; todo implement flow in make

--- tests/sandbox/std=/compile_test.sh
+++ tests/sandbox/std=/compile_test.sh
@@ -4,9 +4,7 @@ CFLAGS='-std=c++98 -pedantic'
 CC=clang++
 FOLDER=$COMPILE_FOLDER
 
-#makefile_rule=...
-
-$CC $CFLAGS *.cpp 2>&1
+$CC $CFLAGS *.cpp 2> compile.log
 
 if [[ $? != 0 ]]; then
 	echo 'ERROR'
