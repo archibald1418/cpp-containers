6dda0d32fce19488a8e93485d6bd8ad3d14a2df8
ldonita-samsung <liam141810@gmail.com>
2022-11-06 00:23:01 +0000

    test for alloc-ing 0 bytes; c++ allocates 1 byte

--- tests/map/memmmm.cpp
+++ tests/map/memmmm.cpp
@@ -1,6 +1,14 @@
+#include <iostream>
+
 int main(){
 
-	new int[0];
+	int* A[10];
+
+	for (int i = 0; i < 10; i++){
+		int *p = new int[0];
+		delete[] p;
+		/* A[i] = new int[0]; */
+		/* *A[i] = '*'; */
+	}
 	return 0;
-	
 }
